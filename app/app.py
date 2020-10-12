4ce37869f0b368035ccec40543b6acd0016f6cd8
Micah <micahjank@gmail.com>
2020-10-12 16:32:46 -0500

    started getting things to show up

--- app/app.py
+++ app/app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
+import requests
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello there, World!'
+def index():
+    response = requests.get('https://my.api.mockaroo.com/seats?key=0523bb20')
 
-@app.route('/<name>')
-def hello_name(name):
-    return f"Hello {name}!"
+    seats = response.json()
+    # i seperate them into two halfs to be able to seperate them in the layout in the html and css
+    top_half = seats[:len(seats)//2]
+    bot_half = seats[len(seats)//2:]
+    return render_template('index.html', top_half=top_half, bot_half=bot_half)
+    
+
+# @app.route('/<name>')
+# def index(name):
+    
 
 
 if __name__ == '__main__':
