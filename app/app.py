6d07537251e9ffceba5c6ee5f18e2dea43b1b07d
MicahJank <MicahJank@users.noreply.github.com>
2020-10-13 12:13:37 -0500

    restructured some of the code and css

--- app/app.py
+++ app/app.py
@@ -4,25 +4,24 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+response = requests.get('https://my.api.mockaroo.com/seats?key=0523bb20')
+seats = response.json()
+left = []
+middle = [] 
+right = [] 
+
+# data needs to be split between left right and middle to display it properly
+for seat in seats:
+    if seat['row_number'] == 'A' or seat['row_number'] == 'B':
+        left.append(seat)
+    elif seat['row_number'] == 'C' or seat['row_number'] == 'D' or seat['row_number'] == 'E':
+        middle.append(seat)
+    else:
+        right.append(seat)
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    response = requests.get('https://my.api.mockaroo.com/seats?key=0523bb20')
-
-    seats = response.json()
-    left = []
-    middle = [] 
-    right = [] 
-
-    # data needs to be split between left right and middle to display it properly
-    for seat in seats:
-        if seat['row_number'] == 'A' or seat['row_number'] == 'B':
-            left.append(seat)
-        elif seat['row_number'] == 'C' or seat['row_number'] == 'D' or seat['row_number'] == 'E':
-            middle.append(seat)
-        else:
-            right.append(seat)
-
-    return render_template('index.html', left=left, right=right, middle=middle)
+    return render_template('index.html', left=left, right=right, middle=middle, value="Hello")
     
 
 # @app.route('/<name>')
